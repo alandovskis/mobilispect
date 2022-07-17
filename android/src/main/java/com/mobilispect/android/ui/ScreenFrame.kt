b02c6ed635538dab9b20f842b2238632276b3d4c
Alex Landovskis <alex@landovskis.com>
2022-07-17 18:32:28 -0400

    TopApBar: Use primary colour for background

--- android/src/main/java/com/mobilispect/android/ui/ScreenFrame.kt
+++ android/src/main/java/com/mobilispect/android/ui/ScreenFrame.kt
@@ -1,10 +1,13 @@
 package com.mobilispect.android.ui
 
+import android.content.res.Configuration
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.mobilispect.android.ui.theme.MobilispectTheme
 
 @Composable
 fun ScreenFrame(screenTitle: String, function: @Composable () -> Unit) {
@@ -15,10 +18,20 @@ fun ScreenFrame(screenTitle: String, function: @Composable () -> Unit) {
 
 @Composable
 fun TopBar(screenTitle: String) {
-    TopAppBar(title = {
+    TopAppBar(backgroundColor = MaterialTheme.colors.primary, title = {
         Text(
             text = screenTitle,
             style = MaterialTheme.typography.h5
         )
     })
+}
+
+@Preview(name = "Light Mode", uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
+@Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun PreviewScreenFrame() {
+    MobilispectTheme {
+        ScreenFrame(screenTitle = "Title") {
+        }
+    }
 }
