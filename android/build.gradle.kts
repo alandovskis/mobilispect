ed64be80df0d6d92ce26e5072c190842caf0ae74
Alex Landovskis <alex@landovskis.com>
2022-08-07 00:11:15 -0400

    build: Add compose test

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,15 @@ android {
         targetSdk = 32
         versionCode = 1
         versionName = "1.0"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     buildTypes {
         getByName("release") {
@@ -50,33 +59,34 @@ dependencies {
     //endregion
 
     //region UI
-    implementation("androidx.activity:activity-compose:1.5.0")
-    implementation("androidx.compose.ui:ui:1.1.1")
+    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.compose.ui:ui:1.2.0")
     // Tooling support (Previews, etc.)
-    implementation("androidx.compose.ui:ui-tooling:1.1.1")
+    implementation("androidx.compose.ui:ui-tooling:1.2.0")
     // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
-    implementation("androidx.compose.foundation:foundation:1.1.1")
+    implementation("androidx.compose.foundation:foundation:1.2.0")
     // Material Design
-    implementation("androidx.compose.material:material:1.1.1")
+    implementation("androidx.compose.material:material:1.2.0")
     // Material design icons
-    implementation("androidx.compose.material:material-icons-core:1.1.1")
-    implementation("androidx.compose.material:material-icons-extended:1.1.1")
+    implementation("androidx.compose.material:material-icons-core:1.2.0")
+    implementation("androidx.compose.material:material-icons-extended:1.2.0")
     // Integration with activities
-    implementation("androidx.activity:activity-compose:1.5.0")
+    implementation("androidx.activity:activity-compose:1.5.1")
     // Integration with ViewModels
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
     // Integration with observables
-    implementation("androidx.compose.runtime:runtime-livedata:1.1.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.runtime:runtime-livedata:1.2.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.2.0")
 
     //region Architecture Components
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     //endregion
 
     // UI Tests
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:${rootProject.extra["compose_version"]}")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.2.0")
     //endregion
 }
 
