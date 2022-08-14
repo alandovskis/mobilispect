55f9421c465971d7e57709c97b4c3f7fa24bdc24
Alex Landovskis <alex@landovskis.com>
2022-08-14 15:35:54 -0400

    test(FrequencyCommitmentViewModel): Complete

--- android/src/test/java/com/mobilispect/android/ui/frequency_commitment/FrequencyCommitmentViewModelTest.kt
+++ android/src/test/java/com/mobilispect/android/ui/frequency_commitment/FrequencyCommitmentViewModelTest.kt
@@ -6,6 +6,7 @@ import com.mobilispect.common.data.agency.AgencyRef
 import com.mobilispect.common.data.frequency_commitment.FrequencyCommitment
 import com.mobilispect.common.data.frequency_commitment.FrequencyCommitmentItem
 import com.mobilispect.common.data.frequency_commitment.TestFrequencyCommitmentRepository
+import com.mobilispect.common.data.route.Route
 import com.mobilispect.common.data.route.RouteRef
 import com.mobilispect.common.data.route.TestRouteRepository
 import com.mobilispect.common.data.time.WEEKDAYS
@@ -24,10 +25,7 @@ class FrequencyCommitmentViewModelTest {
     @get:Rule
     val mainDispatcherRule = MainDispatcherRule()
 
-    private val agencyRef: AgencyRef
-        get() {
-            return AgencyRef(geohash = "abcd", "ABCD")
-        }
+    private val agencyRef: AgencyRef = AgencyRef(geohash = "abcd", "ABCD")
 
     private val routeRepository = TestRouteRepository()
     private val frequencyCommitmentRepository = TestFrequencyCommitmentRepository()
@@ -50,7 +48,7 @@ class FrequencyCommitmentViewModelTest {
     }
 
     @Test
-    fun commitmentFound_bothDirections() = runTest {
+    fun commitmentFound_bothDirections_routeNotFound() = runTest {
         val agency = agencyRef
         val expectedDirections = com.mobilispect.common.data.frequency_commitment.DirectionTime.both(
             start = LocalTime.of(6, 0),
@@ -86,6 +84,59 @@ class FrequencyCommitmentViewModelTest {
                 assertThat(uiItem.frequency).isEqualTo(10)
                 assertThat(uiItem.daysOfTheWeek).isEqualTo(commitmentItem.daysOfWeek)
                 assertThat(uiItem.routes).isEmpty()
+            }
+        }
+
+        collectJob.cancel()
+    }
+
+    @Test
+    fun commitmentFound_bothDirections_routeFound() = runTest {
+        val route = Route(
+            id = RouteRef("was", "found"),
+            shortName = "Found",
+            longName = "Really Found"
+        )
+        routeRepository.insert(route)
+
+        val agency = agencyRef
+        val expectedDirections = com.mobilispect.common.data.frequency_commitment.DirectionTime.both(
+            start = LocalTime.of(6, 0),
+            end = LocalTime.of(21, 0)
+        )
+        val commitmentItem = FrequencyCommitmentItem(
+            daysOfWeek = WEEKDAYS, frequency = Duration.ofMinutes(10),
+            directions = expectedDirections,
+            routes = listOf(route.id)
+        )
+        val commitment = FrequencyCommitment(
+            spans = listOf(
+                commitmentItem
+            ),
+            agency = agency
+        )
+        frequencyCommitmentRepository.insert(commitment)
+
+        val collectJob = launch(UnconfinedTestDispatcher()) {
+            subject.uiState(agency).collect { uiState ->
+                val ui = uiState as CommitmentFound
+                assertThat(ui.items).hasSize(1)
+
+                val uiItem = ui.items.first()
+
+                assertThat(uiItem.directions).hasSize(1)
+                val actualDirection = uiItem.directions.first()
+                val expectedDirection = expectedDirections.first()
+                assertThat(actualDirection.startTime).isEqualTo(expectedDirection.start)
+                assertThat(actualDirection.endTime).isEqualTo(expectedDirection.end)
+                assertThat(actualDirection.isBothDirections).isEqualTo(true)
+
+                assertThat(uiItem.frequency).isEqualTo(10)
+                assertThat(uiItem.daysOfTheWeek).isEqualTo(commitmentItem.daysOfWeek)
+
+                assertThat(uiItem.routes).hasSize(1)
+                val actualRoute = uiItem.routes.first()
+                assertThat(actualRoute.route).isEqualTo("${route.shortName}: ${route.longName}")
             }
         }
 
