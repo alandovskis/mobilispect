8060bfadc73a4c157be69492e14291e726eb62b7
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-01-13 23:23:18 -0500

    Revert "Bump Kotlin to 1.8.0"
    This reverts commit 346d08df69805ae304aad553e59706db97c98536.

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         classpath("com.android.tools.build:gradle:7.3.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
     }
