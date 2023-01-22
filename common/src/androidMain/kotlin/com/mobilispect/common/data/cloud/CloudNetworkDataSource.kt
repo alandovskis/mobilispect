0f4830567db2eea0ed83e1b805d8d4ddccf8d7ac
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-01-22 13:17:18 -0500

    CloudNetworkDataSource: Fix compilation failure

--- common/src/androidMain/kotlin/com/mobilispect/common/data/cloud/CloudNetworkDataSource.kt
+++ common/src/androidMain/kotlin/com/mobilispect/common/data/cloud/CloudNetworkDataSource.kt
@@ -1,9 +1,7 @@
 package com.mobilispect.common.data.cloud
 
 import com.mobilispect.common.data.agency.Agency
-import com.mobilispect.common.data.agency.AgencyRef
 import com.mobilispect.common.data.agency.STM_ID
-import com.mobilispect.common.data.route.Route
 import javax.inject.Inject
 
 private val STM = Agency(
@@ -13,7 +11,4 @@ private val STM = Agency(
 
 class CloudNetworkDataSource @Inject constructor() : NetworkDataSource {
     override suspend fun agencies(): Collection<Agency> = listOf(STM)
-    override suspend fun routes(agency: AgencyRef): Collection<Route> {
-        TODO("Not yet implemented")
-    }
 }
