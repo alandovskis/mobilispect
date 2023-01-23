f96f30794c6ec9d16eb30f080257343433c176e0
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-01-23 00:23:44 -0500

    build: Add detekt

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.0"
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
+	id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
 
 group = "com.mobilispect"
