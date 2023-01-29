263a462b50b89d2207aecb601099d7c6ca514a9a
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-01-29 14:41:42 -0500

    build: Exclude Kotlin intrinsics from mutation testing

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.2"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.8.0"
-	kotlin("plugin.spring") version "1.8.0"
-	id("io.gitlab.arturbosch.detekt") version "1.22.0"
-	id("info.solidsoft.pitest") version "1.7.0"
+    id("org.springframework.boot") version "3.0.2"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.8.0"
+    kotlin("plugin.spring") version "1.8.0"
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
+    id("info.solidsoft.pitest") version "1.7.0"
 }
 
 group = "com.mobilispect"
@@ -14,35 +14,36 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-	implementation("org.springframework.boot:spring-boot-starter-data-rest")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.testcontainers:junit-jupiter:1.17.6")
-	testImplementation("org.testcontainers:mongodb:1.17.6")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.testcontainers:junit-jupiter:1.17.6")
+    testImplementation("org.testcontainers:mongodb:1.17.6")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 pitest {
-	//setProperty("junit5PluginVersion", "0.12")
-	//setProperty("testPlugin", "junit5")
-	setProperty("outputFormats", listOf("HTML"))
-	//setProperty("threads", 2)
-	//setProperty("withHistory", true)
+    //setProperty("junit5PluginVersion", "0.12")
+    //setProperty("testPlugin", "junit5")
+    setProperty("outputFormats", listOf("HTML"))
+    //setProperty("threads", 2)
+    //setProperty("withHistory", true)
+    setProperty("avoidCallsTo", listOf("kotlin.jvm.internal"))
 }
