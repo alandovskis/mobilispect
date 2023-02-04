cbf9946362423ec73bc390ab935154514bf9da3b
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-02-04 12:13:50 -0500

    Agency: Suppress constructor naming

--- api/src/main/kotlin/com/mobilispect/api/Agency.kt
+++ api/src/main/kotlin/com/mobilispect/api/Agency.kt
@@ -2,6 +2,6 @@ package com.mobilispect.api
 
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Suppress("PropertyName") // For _id
+@Suppress("PropertyName", "ConstructorParameterNaming") // For _id
 @Document(value = "agencies")
 data class Agency(val _id: String, val name: String)
