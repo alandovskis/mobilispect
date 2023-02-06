991cebd1dadcdb48b17824963c0265dfcf58b980
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-02-05 19:54:42 -0500

    style(AgencyRepository): Re-format

--- api/src/main/kotlin/com/mobilispect/api/AgencyRepository.kt
+++ api/src/main/kotlin/com/mobilispect/api/AgencyRepository.kt
@@ -14,14 +14,14 @@ interface AgencyRepository : Repository<Agency, String> {
     fun findAll(): List<Agency>
 
     /**
-     * Saves a given entity. Use the returned instance for further operations as the save operation might have changed the
-     * entity instance completely.
+     * Saves a given entity. Use the returned instance for further operations as the save operation might have changed
+     * the entity instance completely.
      *
      * @param entity must not be null.
      * @return the saved entity; will never be null.
      * @throws IllegalArgumentException in case the given entity is null.
-     * @throws OptimisticLockingFailureException when the entity uses optimistic locking and has a version attribute with
-     * a different value from that found in the persistence store. Also thrown if the entity is assumed to be
+     * @throws OptimisticLockingFailureException when the entity uses optimistic locking and has a version attribute
+     * with a different value from that found in the persistence store. Also thrown if the entity is assumed to be
      * present but does not exist in the database.
      */
     fun save(entity: Agency): Agency
