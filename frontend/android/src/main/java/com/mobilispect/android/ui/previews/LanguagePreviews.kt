4995f61cc57e9b1c2d26285d29971a4b71fd2ac7
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-02-28 14:06:59 -0500

    Add LanguagePreviews

---
+++ frontend/android/src/main/java/com/mobilispect/android/ui/previews/LanguagePreviews.kt
@@ -0,0 +1,7 @@
+package com.mobilispect.android.ui.previews
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(locale = "en", name = "English")
+@Preview(locale = "fr", name = "French")
+annotation class MultiLanguagePreview
