e22eaaef62e638df82dcc2bd364ed564c38cff56
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-03-08 20:17:04 -0500

    style(RouteAPIIntegrationTest): Remove wildcard import

--- backend/src/test/kotlin/com/mobilispect/backend/api/RouteAPIIntegrationTest.kt
+++ backend/src/test/kotlin/com/mobilispect/backend/api/RouteAPIIntegrationTest.kt
@@ -1,6 +1,11 @@
 package com.mobilispect.backend.api
 
-import com.mobilispect.backend.data.*
+import com.mobilispect.backend.data.Agency
+import com.mobilispect.backend.data.HeadwayEntry
+import com.mobilispect.backend.data.MongoDBInitializer
+import com.mobilispect.backend.data.Route
+import com.mobilispect.backend.data.RouteRepository
+import com.mobilispect.backend.data.createMongoDBContainer
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
