fbc49af906ab9ed9cf414f6d49ee130152b95056
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-03-16 21:40:28 -0400

    Move RouteRepository to data.route

--- backend/src/main/kotlin/com/mobilispect/backend/data/route/RouteRepository.kt
+++ backend/src/main/kotlin/com/mobilispect/backend/data/route/RouteRepository.kt
@@ -1,6 +1,5 @@
-package com.mobilispect.backend.data
+package com.mobilispect.backend.data.route
 
-import com.mobilispect.backend.data.route.Route
 import org.springframework.dao.OptimisticLockingFailureException
 import org.springframework.data.repository.Repository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
