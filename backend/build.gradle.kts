48ff364c573bff67b9ea3e5694d608606fe0932b
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-03-18 00:10:42 -0400

    build: Remove unused jackson module

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -25,7 +25,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation(libs.kotlinx.serialization)
