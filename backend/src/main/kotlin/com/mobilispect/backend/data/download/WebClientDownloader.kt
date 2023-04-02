e01ead110e167d57454882c811fff29483990f7b
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-04-02 16:01:16 -0400

    WebClientDownloader now implements retry

--- backend/src/main/kotlin/com/mobilispect/backend/data/download/WebClientDownloader.kt
+++ backend/src/main/kotlin/com/mobilispect/backend/data/download/WebClientDownloader.kt
@@ -2,9 +2,13 @@ package com.mobilispect.backend.data.download
 
 import org.springframework.core.io.buffer.DataBuffer
 import org.springframework.core.io.buffer.DataBufferUtils
+import org.springframework.http.HttpStatus
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.WebClientRequestException
+import org.springframework.web.reactive.function.client.WebClientException
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import reactor.util.retry.Retry
 import java.nio.file.StandardOpenOption
+import java.time.Duration
 
 /**
  * A [Downloader] that uses Spring [WebClient].
@@ -15,12 +19,24 @@ internal class WebClientDownloader(webClientBuilder: WebClient.Builder) : Downlo
     override fun download(url: String): Result<String> {
         val dest = kotlin.io.path.createTempFile()
         return try {
-            val dataBuffer = webClient.get().uri(url).retrieve().bodyToFlux(DataBuffer::class.java)
+            val dataBuffer = webClient.get()
+                .uri(url)
+                .retrieve()
+                .bodyToFlux(DataBuffer::class.java)
+                .retryWhen(Retry.backoff(3, Duration.ofSeconds(1))
+                    .filter { error ->
+                        val responseError = error as? WebClientResponseException
+                        val serverError = responseError?.statusCode?.is5xxServerError ?: false
+                        val rateLimitedError =
+                            responseError?.statusCode?.isSameCodeAs(HttpStatus.TOO_MANY_REQUESTS) ?: false
+                        return@filter serverError || rateLimitedError
+                    })
+
             DataBufferUtils.write(
                 dataBuffer, dest, StandardOpenOption.CREATE
             ).share().block()
-            Result.success(dest.toString())
-        } catch (e: WebClientRequestException) {
+            return Result.success(dest.toString())
+        } catch (e: WebClientException) {
             Result.failure(e)
         }
     }
