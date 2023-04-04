cbbbf0aea957ff4bb89a446c9360839fa211af66
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-04-04 00:24:58 -0400

    test(GTFSAgencyDataSource): Re-format

--- backend/src/test/kotlin/com/mobilispect/backend/data/gtfs/GTFSAgencyDataSourceTest.kt
+++ backend/src/test/kotlin/com/mobilispect/backend/data/gtfs/GTFSAgencyDataSourceTest.kt
@@ -1,6 +1,7 @@
 package com.mobilispect.backend.data.gtfs
 
 import com.mobilispect.backend.data.agency.Agency
+import com.mobilispect.backend.util.copyResourceTo
 import kotlinx.serialization.SerializationException
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -8,50 +9,42 @@ import org.junit.jupiter.api.io.TempDir
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.core.io.ResourceLoader
-import java.io.File
 import java.io.IOException
 import java.nio.file.Path
+
+private const val VERSION = "v1"
 
 @SpringBootTest
 internal class GTFSAgencyDataSourceTest {
     @Autowired
     lateinit var resourceLoader: ResourceLoader
 
+    private val subject = GTFSAgencyDataSource()
+
     @Test
     fun fileNotFound(@TempDir root: Path) {
-        val version = "v1"
-        val subject = GTFSAgencyDataSource()
-
-        val result = subject.agencies(root.toString(), version).exceptionOrNull()!!
+        val result = subject.agencies(root.toString(), VERSION).exceptionOrNull()!!
 
         assertThat(result).isInstanceOf(IOException::class.java)
     }
 
     @Test
     fun corrupted(@TempDir root: Path) {
-        val inputStream = resourceLoader.getResource("classpath:citpi-agency-corrupt.txt").file.inputStream()
-        val outputStream = File(root.toFile(), "agency.txt").outputStream()
-        inputStream.copyTo(outputStream, 1024)
-        val version = "v1"
-        val subject = GTFSAgencyDataSource()
+        resourceLoader.copyResourceTo(src = "classpath:citpi-agency-corrupt.txt", root = root, dst = "agency.txt")
 
-        val result = subject.agencies(root.toString(), version).exceptionOrNull()
+        val result = subject.agencies(root.toString(), VERSION).exceptionOrNull()
 
         assertThat(result).isInstanceOf(SerializationException::class.java)
     }
 
     @Test
     fun importsSuccessfully(@TempDir root: Path) {
-        val inputStream = resourceLoader.getResource("classpath:citpi-agency.txt").file.inputStream()
-        val outputStream = File(root.toFile(), "agency.txt").outputStream()
-        inputStream.copyTo(outputStream, 1024)
-        val version = "v1"
-        val subject = GTFSAgencyDataSource()
+        resourceLoader.copyResourceTo(src = "classpath:citpi-agency.txt", root = root, dst = "agency.txt")
 
-        val agencies = subject.agencies(root.toString(), version).getOrNull()!!
+        val agencies = subject.agencies(root.toString(), VERSION).getOrNull()!!
 
         assertThat(agencies).contains(
-            Agency(_id = "CITPI", name = "exo-La Presqu'île", version = version)
+            Agency(_id = "CITPI", name = "exo-La Presqu'île", version = VERSION)
         )
     }
 }
