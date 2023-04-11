79f14d22c678e92d28e574d009083d6a3d6c7387
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-04-11 00:26:41 -0400

    TransitLandClient: Remove unused routes and stops

--- backend/src/main/kotlin/com/mobilispect/backend/data/stop/StopResult.kt
+++
@@ -1,3 +0,0 @@
-package com.mobilispect.backend.data.stop
-
-class StopResult(val stops: Collection<Stop>, val after: Int)
--- backend/src/main/kotlin/com/mobilispect/backend/data/transit_land/RouteResult.kt
+++
@@ -1,8 +0,0 @@
-package com.mobilispect.backend.data.transit_land
-
-import com.mobilispect.backend.data.route.Route
-
-/**
- * The combination of routes extracted and any paging parameters.
- */
-class RouteResult(val routes: Collection<Route>, val after: Int)
