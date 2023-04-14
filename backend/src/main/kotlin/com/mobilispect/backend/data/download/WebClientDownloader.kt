b3512fa65588aad0aa245053258e126a04041f42
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-04-14 12:02:05 -0400

    WebClientDownloader: Follow redirects

--- backend/src/main/kotlin/com/mobilispect/backend/data/download/WebClientDownloader.kt
+++ backend/src/main/kotlin/com/mobilispect/backend/data/download/WebClientDownloader.kt
@@ -3,9 +3,11 @@ package com.mobilispect.backend.data.download
 import org.springframework.core.io.buffer.DataBuffer
 import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.http.HttpStatus
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientException
 import org.springframework.web.reactive.function.client.WebClientResponseException
+import reactor.netty.http.client.HttpClient
 import reactor.util.retry.Retry
 import java.nio.file.StandardOpenOption
 import java.time.Duration
@@ -16,7 +18,13 @@ private const val RETRY_ATTEMPTS = 3L
  * A [Downloader] that uses Spring [WebClient].
  */
 internal class WebClientDownloader(webClientBuilder: WebClient.Builder) : Downloader {
-    private var webClient: WebClient = webClientBuilder.build()
+    private var webClient: WebClient = webClientBuilder
+        .clientConnector(
+            ReactorClientHttpConnector(
+                HttpClient.create().followRedirect(true)
+            )
+        )
+        .build()
 
     override fun download(url: String): Result<String> {
         val dest = kotlin.io.path.createTempFile()
@@ -25,8 +33,9 @@ internal class WebClientDownloader(webClientBuilder: WebClient.Builder) : Downlo
                 .uri(url)
                 .retrieve()
                 .bodyToFlux(DataBuffer::class.java)
-                .retryWhen(Retry.backoff(RETRY_ATTEMPTS, Duration.ofSeconds(1))
-                    .filter { error ->
+                .retryWhen(
+                    Retry.backoff(RETRY_ATTEMPTS, Duration.ofSeconds(1))
+                        .filter { error ->
                         val responseError = error as? WebClientResponseException
                         val serverError = responseError?.statusCode?.is5xxServerError ?: false
                         val rateLimitedError =
