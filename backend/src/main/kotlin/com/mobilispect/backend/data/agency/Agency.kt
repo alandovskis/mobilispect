235db08b183c327f4155c657a730f1845443c2e1
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-04-16 00:36:22 -0400

    Agency now enforces that ID is in OneStop ID format

--- backend/src/main/kotlin/com/mobilispect/backend/data/agency/Agency.kt
+++ backend/src/main/kotlin/com/mobilispect/backend/data/agency/Agency.kt
@@ -3,6 +3,19 @@ package com.mobilispect.backend.data.agency
 import com.mobilispect.backend.batch.Entity
 import org.springframework.data.mongodb.core.mapping.Document
 
+private val ONESTOP_ID_REGEX = Regex("o-[a-z0-9]+-[a-z0-9]+")
+
 @Suppress("ConstructorParameterNaming") // For _id
 @Document(value = "agencies")
-data class Agency(override val _id: String, val name: String, override val version: String) : Entity
+data class Agency(
+    /**
+     * ID (in OneStop ID format ex: o-geohash-name)
+     */
+    override val _id: String,
+    val name: String,
+    override val version: String
+) : Entity {
+    init {
+        require(ONESTOP_ID_REGEX.matches(_id)) { "Must be in OneStopID format i.e. o-geohash-name" }
+    }
+}
---
+++ backend/src/test/kotlin/com/mobilispect/backend/data/agency/AgencyTest.kt
@@ -0,0 +1,26 @@
+package com.mobilispect.backend.data.agency
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+
+class AgencyTest {
+    @Test
+    fun agencyNotCreatedIfIDMissingGeoHash() {
+        assertThrows<IllegalArgumentException> {
+            Agency(_id = "o--a", name = "name", version = "version")
+        }
+    }
+
+    @Test
+    fun agencyNotCreatedIfIDMissingName() {
+        assertThrows<IllegalArgumentException> {
+            Agency(_id = "o-a-", name = "name", version = "version")
+        }
+    }
+
+    @Test
+    fun agencyIsCreatedIfValidID() {
+        Agency(_id = "o-a-b", name = "name", version = "version")
+    }
+}
