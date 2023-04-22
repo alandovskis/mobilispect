39f08c5fda010a36a5208e260ddcb9d83483335f
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-04-22 17:27:45 -0400

    Remove UTF-8 BOM for GTFS feeds

--- backend/src/main/kotlin/com/mobilispect/backend/util/FileExt.kt
+++ backend/src/main/kotlin/com/mobilispect/backend/util/FileExt.kt
@@ -3,6 +3,10 @@ package com.mobilispect.backend.util
 import java.io.File
 
 /**
- * Ensure that newlines are normalized to Unix-style.
+ * Ensure that newlines are normalized to Unix-style and no leading/trailing whitespace.
  */
-fun File.readTextAndNormalize(): String = readText().replace("\r\n", "\n")
+fun File.readTextAndNormalize(): String = readText()
+    .replace("\r\n", "\n")
+    .trim()
+    .replace("\u00a0", "")
+    .replace("\ufeff", "")
