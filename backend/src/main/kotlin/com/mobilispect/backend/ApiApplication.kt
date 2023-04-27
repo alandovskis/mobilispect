2dd980f5e933e79848f064ade3af506662e7a45d
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-04-26 21:16:25 -0400

    doc: Describe API

--- backend/src/main/kotlin/com/mobilispect/backend/ApiApplication.kt
+++ backend/src/main/kotlin/com/mobilispect/backend/ApiApplication.kt
@@ -1,9 +1,18 @@
 package com.mobilispect.backend
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = Info(
+		title = "Mobilispect API",
+		description = "API Definitions for the Mobility Inspector",
+		version = "1"
+	)
+)
 class ApiApplication
 
 @Suppress("SpreadOperator")
