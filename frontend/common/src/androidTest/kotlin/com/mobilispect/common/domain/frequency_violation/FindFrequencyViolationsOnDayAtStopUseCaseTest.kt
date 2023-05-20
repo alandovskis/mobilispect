e643793b7f3d3653fc3c1273cc711ac0aa401e34
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-05-20 17:53:23 -0400

    test(FindFrequencyViolations): Fix compilation error

--- frontend/common/src/androidTest/kotlin/com/mobilispect/common/domain/frequency_violation/FindFrequencyViolationsOnDayAtStopUseCaseTest.kt
+++ frontend/common/src/androidTest/kotlin/com/mobilispect/common/domain/frequency_violation/FindFrequencyViolationsOnDayAtStopUseCaseTest.kt
@@ -1,7 +1,6 @@
 package com.mobilispect.common.domain.frequency_violation
 
 import com.mobilispect.common.data.frequency_commitment.STM_FREQUENCY_COMMITMENT
-import com.mobilispect.common.data.route.RouteRef
 import com.mobilispect.common.data.schedule.Direction
 import com.mobilispect.common.data.schedule.FakeScheduleRepository
 import org.junit.Test
@@ -13,7 +12,7 @@ internal class FindFrequencyViolationsOnDayAtStopUseCaseTest {
         FindFrequencyViolationsOnDayAtStopUseCase(FakeScheduleRepository())
             .invoke(
                 start = LocalDateTime.of(2022, 7, 7, 6, 0, 0),
-                routeRef = RouteRef(geohash = "f25em", routeNumber = "141"),
+                routeRef = "r-f25em-141",
                 stopRef = com.mobilispect.common.data.stop.StopRef(geohash = "abcd", name = "test"),
                 direction = Direction.Outbound,
                 commitment = STM_FREQUENCY_COMMITMENT
