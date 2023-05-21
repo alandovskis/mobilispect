e1f7ca2ce4082cea057726be5714a39cb4cc9880
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-05-20 23:24:35 -0400

    OneStopAgencyID: Use backticks

--- backend/src/main/kotlin/com/mobilispect/backend/data/agency/OneStopAgencyID.kt
+++ backend/src/main/kotlin/com/mobilispect/backend/data/agency/OneStopAgencyID.kt
@@ -7,7 +7,7 @@ private val ONESTOP_ID_REGEX = Regex("o-[a-z0-9]+-[a-z0-9~çéâêîôûàèùë
  */
 class OneStopAgencyID(internal val str: String) {
     init {
-        require(ONESTOP_ID_REGEX.matches(str)) { "Must be in OneStopID format i.e. o-geohash-name. Was $str" }
+        require(ONESTOP_ID_REGEX.matches(str)) { "Must be in OneStopID format i.e. o-geohash-name. Was `$str`" }
     }
 
     override fun equals(other: Any?): Boolean {
