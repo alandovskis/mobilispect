b6e7840dfb1d08e6ef6aa3a1a1751ad1365649d1
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-05-20 21:40:34 -0400

    build: Replace packagingOptions with packaging

--- frontend/android/build.gradle.kts
+++ frontend/android/build.gradle.kts
@@ -42,7 +42,7 @@ android {
             isDebuggable = false
         }
     }
-    packagingOptions {
+    packaging {
         resources {
             excludes += "META-INF/AL2.0"
             excludes += "META-INF/LGPL2.1"
