7f7c4c022227c8b4e462a886f50b70112ca45d05
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2023-11-26 18:18:48 -0500

    build: Extract remaining dependencies to version catalog

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -9,9 +9,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
-        classpath("com.android.tools.build:gradle:8.1.1")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.gradle)
+        classpath(libs.hilt.android.gradle.plugin)
     }
 }
 
