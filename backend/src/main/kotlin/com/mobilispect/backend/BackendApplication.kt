4b4ecdadee0571e899a1017654784ea867726e89
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2025-06-29 14:12:17 -0400

    Scan all packages

--- backend/src/main/kotlin/com/mobilispect/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/mobilispect/backend/BackendApplication.kt
@@ -3,7 +3,13 @@ package com.mobilispect.backend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = [
+	"com.mobilispect.backend.schedule.agency",
+	"com.mobilispect.backend.schedule.feed",
+	"com.mobilispect.backend.schedule.transit_land.api",
+	"com.mobilispect.backend.schedule.transit_land.internal.client",
+	"com.mobilispect.backend.schedule.transit_land.internal.credentials",
+])
 class BackendApplication
 
 fun main(args: Array<String>) {
