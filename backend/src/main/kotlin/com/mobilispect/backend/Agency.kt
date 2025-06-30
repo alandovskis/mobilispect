ff127544bba17506f07b6f1f7b5b0d24484b85fd
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2025-06-29 22:51:31 -0400

    Routes API is working

--- backend/src/main/kotlin/com/mobilispect/backend/Agency.kt
+++ backend/src/main/kotlin/com/mobilispect/backend/Agency.kt
@@ -1,8 +1,9 @@
 package com.mobilispect.backend
 
+import kotlinx.serialization.Serializable
 import org.springframework.data.mongodb.core.mapping.Document
 
-private val ONESTOP_ID_REGEX = Regex("o(-[a-z0-9]+)?-[a-z0-9~çéâêîôûàèùëïü]+")
+val AGENCY_ONESTOP_ID_REGEX = Regex("o(-[a-z0-9]+)?-[a-z0-9~çéâêîôûàèùëïü]+")
 
 @Document(value = "agencies")
 data class Agency(
@@ -21,7 +22,7 @@ data class Agency(
     val versions: Collection<String>
 ) {
     init {
-        require(uid.matches(ONESTOP_ID_REGEX)) { "Must be in OneStopID format i.e. o[-geohash]-name. Was $uid" }
+        require(uid.matches(AGENCY_ONESTOP_ID_REGEX)) { "Must be in OneStopID format i.e. o[-geohash]-name. Was $uid" }
         require(localID.isNotBlank()) { "Local ID must not be blank." }
         require(name.isNotBlank()) { "Name must not be blank." }
         require(versions.isNotEmpty()) { "Versions must not be empty." }
---
+++ backend/src/main/kotlin/com/mobilispect/backend/RouteRepository.kt
@@ -0,0 +1,30 @@
+package com.mobilispect.backend
+
+import org.springframework.dao.OptimisticLockingFailureException
+import org.springframework.data.repository.Repository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.data.rest.core.annotation.RestResource
+
+@RepositoryRestResource(path = "routes")
+interface RouteRepository : Repository<Route, String> {
+    /**
+     * Returns all instances of the type.
+     *
+     * @return all entities
+     */
+    fun findAll(): List<Route>
+
+    @RestResource(exported = false)
+    fun save(entity: Route): Route
+
+    /**
+     * Deletes all entities.
+     *
+     * @throws OptimisticLockingFailureException when at least one entity uses optimistic locking and has a version
+     * attribute with a different value from that found in the persistence store. Also thrown if at least one
+     * entity is assumed to be present but does not exist in the database.
+     */
+    fun deleteAll()
+
+    fun findAllByAgencyID(id: String): List<Route>
+}
