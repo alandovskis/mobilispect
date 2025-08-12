9bdb4a86969e42ba4e446ec74cc6db955404c90d
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2025-08-11 22:32:16 -0400

    frontend: Bump all dependencies

--- frontend/mobile/androidApp/build.gradle.kts
+++ frontend/mobile/androidApp/build.gradle.kts
@@ -8,11 +8,11 @@ plugins {
 
 android {
     namespace = "com.mobilispect.mobile.android"
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.mobilispect.mobile.android"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
